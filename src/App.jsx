fc7bf4634ea888772887637affb8cae78c2b65a0
Hirthuk <sharankumarp02@gmail.com>
2024-12-25 20:34:32 +0530

    I back

--- src/App.jsx
+++ src/App.jsx
@@ -120,77 +120,81 @@ function App() {
           </ul>
         </nav>
         {/* Content */}
-        <section className="content">
-          {/* Left content */}
-          <div className="left-content">
-            {/* Activities */}
-            <div className="activities">
+        <section class="content">
+          {/* Left Content */}
+          <div class="left-content">
+            <div class="activities">
               <h1>Popular Activities</h1>
-              <div className="activity-container">
-                <div className="image-container img-one">
+              <div class="activity-container">
+                <div class="image-container img-one">
                   <img
                     src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/467cf682-03fb-4fae-b129-5d4f5db304dd"
-                    alt="Tennis"
+                    alt="tennis"
                   />
-                  <div className="overlay">
+                  <div class="overlay">
                     <h3>Tennis</h3>
                   </div>
                 </div>
-              </div>
-              <div className="activity-container">
-                <div className="image-container img-two">
+
+                <div class="image-container img-two">
                   <img
                     src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/3bab6a71-c842-4a50-9fed-b4ce650cb478"
-                    alt="Hiking"
+                    alt="hiking"
                   />
-                  <div className="overlay">
+                  <div class="overlay">
                     <h3>Hiking</h3>
                   </div>
                 </div>
-              </div>
-              <div className="activity-container">
-                <div className="image-container img-three">
-                <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c8e88356-8df5-4ac5-9e1f-5b9e99685021" alt="running" />
-                  <div className="overlay">
+
+                <div class="image-container img-three">
+                  <img
+                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/c8e88356-8df5-4ac5-9e1f-5b9e99685021"
+                    alt="running"
+                  />
+                  <div class="overlay">
+                    <h3>Running</h3>
+                  </div>
+                </div>
+
+                <div class="image-container img-four">
+                  <img
+                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/69437d08-f203-4905-8cf5-05411cc28c19"
+                    alt="cycling"
+                  />
+                  <div class="overlay">
                     <h3>Cycling</h3>
                   </div>
                 </div>
-              </div>
-              <div className="activity-container">
-                <div className="image-container img-four">
-                <img src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e1a66078-1927-4828-b793-15c403d06411" alt="yoga" />
-                  <div className="overlay">
+
+                <div class="image-container img-five">
+                  <img
+                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/e1a66078-1927-4828-b793-15c403d06411"
+                    alt="yoga"
+                  />
+                  <div class="overlay">
                     <h3>Yoga</h3>
                   </div>
                 </div>
-              </div>
-              <div className="activity-container">
-                <div className="image-container img-five">
-                  <img
-                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/467cf682-03fb-4fae-b129-5d4f5db304dd"
-                    alt="Tennis"
-                  />
-                  <div className="overlay">
-                    <h3>Badminton</h3>
-                  </div>
-                </div>
-              </div>
 
-              <div className="activity-container">
-                <div className="image-container img-six">
+                <div class="image-container img-six">
                   <img
-                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/467cf682-03fb-4fae-b129-5d4f5db304dd"
-                    alt="Tennis"
+                    src="https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/7568e0ff-edb5-43dd-bff5-aed405fc32d9"
+                    alt="swimming"
                   />
-                  <div className="overlay">
-                    <h3>Skipping</h3>
+                  <div class="overlay">
+                    <h3>Swimming</h3>
                   </div>
                 </div>
               </div>
             </div>
+           
           </div>
+          
+          
           {/* Right content */}
-          <div className="right-content"></div>
+          <div className="right-content">
+
+          </div>
         </section>
       </main>
     </div>
